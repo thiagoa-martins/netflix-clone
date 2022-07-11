3e16d75025d417bde894f9da0fd236742f83faac
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-11 20:11:17 -0300

    :art: Add JoinNetflix in the main

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-import LogoNetflix from "./src/components/LogoNetflix";
+import JoinNetflix from "./src/objects/JoinNetflix";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
   "beforeend",
   `
-    ${LogoNetflix()}
+    ${JoinNetflix()}
   `
 );
 
