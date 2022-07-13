339ba1005fc6543497cec3fcf2c86cbe61b8ac82
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-13 16:40:26 -0300

    :art: Add SignedSection in the main

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-import JoinNetflix from "./src/objects/JoinNetflix";
+import SignedSection from "./src/objects/SignedSection";
+import StoryHeader from "./src/objects/StoryHeader";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
   "beforeend",
   `
-    ${JoinNetflix()}
+   ${SignedSection()}
   `
 );
 
