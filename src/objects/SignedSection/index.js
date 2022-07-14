b35ace2e03ca958535b8f543a907464e9738b56b
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-14 13:38:51 -0300

    :art: Add CreateAccount in the SignedSection

--- src/objects/SignedSection/index.js
+++ src/objects/SignedSection/index.js
@@ -1,3 +1,4 @@
+import CreateAccount from "../../components/CreateAccount";
 import StoryHeader from "../StoryHeader";
 import "./style.css";
 
@@ -5,6 +6,7 @@ function SignedSection() {
     return /*html*/`
         <div class="home-section">
             ${StoryHeader()}
+
             <section class="signed-section">
                 <div class="container">
                     <h1>Filmes, séries e muito mais. Sem limites.</h1>
@@ -12,7 +14,9 @@ function SignedSection() {
                     <h2>Assista onde quiser. Cancele quando quiser.</h2>
 
                     <h3>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</h3>
-                </div>  
+
+                    ${CreateAccount()}
+                </div>     
             </section>
         </div>
     `;
