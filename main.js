956e49e3e843875e33a22eec269c697d3c7da4af
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-19 18:58:14 -0300

    :lipstick: Add CSS of the StoryCards object

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import SignedSection from "./src/objects/SignedSection";
 import EndCard from "./src/components/EndCard";
+import StoryCards from "./src/objects/StoryCards";
 
 const $root = document.querySelector("#root");
 
@@ -10,6 +11,8 @@ $root.insertAdjacentHTML(
   "beforeend",
   `
    ${SignedSection()}
+   ${EndCard()}
+   ${StoryCards()}
    ${EndCard()}
   `
 );
--- src/objects/StoryCards/index.js
+++ src/objects/StoryCards/index.js
@@ -1,12 +1,31 @@
-import "./style.css";
+import "./styles.css";
 
 function StoryCards() {
     return /*html*/`
         <section class="story-cards">
-            <div class="card">
-                <div></div>
-                <img src="#" alt="">
-            </div>
+            <div class="container">
+                <div class="card">
+                    <div class="text">
+                        <h1>Aproveite na TV.</h1>
+                           
+                        <h2>
+                            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.
+                        </h2>
+                    </div>
+                    
+                    <div class="image">
+                        <img src="./src/images/tv.png" alt="Imagem de televisão">
+
+                        <div class="video">
+                            <video autoplay loop>
+                                <source src="./src/videos/video-tv.m4v" type="video/mp4">
+                                Your browser does not support the video tag.
+                                Seu navegador não oferece suporte a essa tag de vídeo.
+                            </video>
+                        </div>
+                    </div>
+                </div>
+            </div>       
         </section>
     `; 
 }
