8f698c16d533fbc49e2cb6d5add43d3964ceea17
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-19 15:44:03 -0300

    :sparkles: Create StoryCards component without CSS

---
+++ src/objects/StoryCards/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function StoryCards() {
+    return /*html*/`
+        <section class="story-cards">
+            <div class="card">
+                <div></div>
+                <img src="#" alt="">
+            </div>
+        </section>
+    `; 
+}
+
+export default StoryCards;
