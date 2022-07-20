08a93d28bfb75cb69801f71cd40309cbc07b8d40
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-20 16:50:02 -0300

    :construction: Add card of the StoryCard object

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ $root.insertAdjacentHTML(
    ${SignedSection()}
    ${EndCard()}
    ${StoryCards()}
-   ${EndCard()}
   `
 );
 
--- src/objects/StoryCards/index.js
+++ src/objects/StoryCards/index.js
@@ -1,3 +1,4 @@
+import EndCard from "../../components/EndCard";
 import "./styles.css";
 
 function StoryCards() {
@@ -25,7 +26,38 @@ function StoryCards() {
                         </div>
                     </div>
                 </div>
-            </div>       
+            </div>
+                
+            ${EndCard()}
+            
+            <div class="container">
+                <div class="card">
+                    <div class="image">
+                        <img src="./src/images/mobile.jpg" alt="Imagem mobile">
+
+                        <div class="animation">
+                            <img src="./src/images/boxshot.png" alt="Stranger Things">
+
+                            <div class="description">
+                                <span>Stranger Things</span>
+                                <div>Download em andamento...</div>
+                            </div>
+                            
+                            <img src="./src/images/download-icon.gif" alt="Ícone de download">
+                        </div>
+                    </div>
+
+                    <div class="text">
+                        <h1>Baixe séries para assistir offline.</h1>
+                           
+                        <h2>
+                        Salve seus títulos favoritos e sempre tenha algo para assistir.
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            
+            ${EndCard()}
         </section>
     `; 
 }
