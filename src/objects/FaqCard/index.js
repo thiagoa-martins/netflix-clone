b5837d71ab1ea9e752b95445b6e81394b298c481
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-21 17:19:07 -0300

    :sparkles: Create FaqCard component with CSS

---
+++ src/objects/FaqCard/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function FaqCard() {
+    return /*html*/`
+        <section class="faq-card">
+            <div class="container">
+                <h1>Perguntas frequentes</h1>
+
+               
+            </div>
+        </section>
+    `;
+}
+
+export default FaqCard;
