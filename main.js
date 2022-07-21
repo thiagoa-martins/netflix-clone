f5721bfe3d8611029b07444d7cdea3ccc77d0abf
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-21 17:23:12 -0300

    :sparkles: Create FaqList component with CSS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import "./src/styles/elements/base.css";
 import SignedSection from "./src/objects/SignedSection";
 import EndCard from "./src/components/EndCard";
 import StoryCards from "./src/objects/StoryCards";
+import FaqCard from "./src/objects/FaqCard";
 
 const $root = document.querySelector("#root");
 
@@ -13,6 +14,7 @@ $root.insertAdjacentHTML(
    ${SignedSection()}
    ${EndCard()}
    ${StoryCards()}
+   ${FaqCard()}
   `
 );
 
---
+++ src/components/FaqList/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function FaqList() {
+    return /*html*/`
+        <ul class="faq-list">
+            <li>
+                <a>O que é a Netflix?</a>
+            </li>
+        </ul>
+    `;
+}
+
+export default FaqList;
