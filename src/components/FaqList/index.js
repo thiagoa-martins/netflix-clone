5cbf39f8b0f910fc8fd0405ce7baa10959b73c55
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-22 15:28:59 -0300

    :lipstick: Add CSS of the FaqList component

--- src/components/FaqList/index.js
+++ src/components/FaqList/index.js
@@ -1,15 +1,26 @@
 import "./style.css";
 
 function FaqList() {
+    window.faqList = {};
+    window.faqList.handleClick = () => {
+        
+    }
+
     return /*html*/`
         <ul class="faq-list">
             <li>
-                <span>O que é a Netflix?</span>
+                <button onClick="handleClick()">
+                    <span>O que é a Netflix?</span>
+                </button>
+
+                <hr>
                 
                 <div class="answer">
                     <p>
                         A Netflix é um serviço de transmissão online que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet.
                     </p>
+
+                    <br>
 
                     <p>
                         Você pode assistir a quantos filmes e séries quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes.
