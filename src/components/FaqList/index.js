2ec8c45ed2a6d485658486c1827744cb44656ff1
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-22 17:16:43 -0300

    :sparkles: Add Javascript of the FaqList to show answers

--- src/components/FaqList/index.js
+++ src/components/FaqList/index.js
@@ -1,21 +1,27 @@
 import "./style.css";
 
 function FaqList() {
+    
+
     window.faqList = {};
-    window.faqList.handleClick = () => {
-        
+    window.faqList.handleClick = (event) => {
+        const $origin = event.target;
+        const $faqList = $origin.closest(".faq-list");
+
+        const $div = $faqList.querySelector("div");
+        $div.classList.toggle("faq-answer");
     }
 
     return /*html*/`
         <ul class="faq-list">
             <li>
-                <button onClick="handleClick()">
+                <button onClick="faqList.handleClick(event)">
                     <span>O que é a Netflix?</span>
                 </button>
 
                 <hr>
                 
-                <div class="answer">
+                <div class="faq-answer answer">
                     <p>
                         A Netflix é um serviço de transmissão online que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet.
                     </p>
@@ -27,7 +33,22 @@ function FaqList() {
                     </p>
                 </div>
             </li>
+        </ul>
 
+        <ul class="faq-list">
+            <li>
+                <button onClick="faqList.handleClick(event)">
+                    <span>Quando custa a Netflix?</span>
+                </button>
+
+                <hr>
+                
+                <div class="faq-answer answer" >
+                    <p>
+                        Assista à Netflix no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$25,90 a R$55,90 por mês. Sem contrato nem taxas extras.
+                    </p>
+                </div>
+            </li>
         </ul>
     `;
 }
